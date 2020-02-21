5bb61894ca3fbf1b8da6dfa368425648daa7046f
jjisol <jjisol97@gmail.com>
2020-02-21 16:42:01 +0900

    modify home.html swiper

--- home/urls.py
+++ home/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.HomePageView, name='home'),
     path('introduction/', views.introduction, name='introduction'),
+    path('yoga/datail/', views.yoga_detail, name='yoga_detail'),
+    path('pilates/datail/', views.pilates_detail, name='pilates_detail'),
 ]
