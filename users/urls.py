fa978d3281a531b77040ea3aa6b1570895da682e
jjisol <jjisol97@gmail.com>
2020-03-04 14:25:56 +0900

    add items to cart

--- users/urls.py
+++ users/urls.py
@@ -4,7 +4,6 @@ from . import views
 from django.conf.urls import url
 
 urlpatterns = [
-    #path('signup/', SignUpView.as_view(), name='signup'),
     path('signup/', views.signup, name='signup'),
     path('login/', views.signin, name='signin'),
     path('mypage/', views.mypage, name='mypage'),
@@ -15,4 +14,13 @@ urlpatterns = [
         views.activate, name='activate'),
     path('update/', views.update, name='update'),
     path('password/', views.password, name='password'),
+    #cart
+    path('cart/', views.cart_list, name='cart_list'),
+    url(r'^add/(?P<id>\d+)/center/$', views.add_to_cart_center, name='add_to_cart_center'),
+    url(r'^add/(?P<id>\d+)/event/$', views.add_to_cart_event, name='add_to_cart_event'),
+    url(r'^add/(?P<id>\d+)/onedayclass/$', views.add_to_cart_onedayclass, name='add_to_cart_onedayclass'),
+    url(r'^del/(?P<id>\d+)/center/$', views.del_from_cart_center, name='del_from_cart_center'),
+    url(r'^del/(?P<id>\d+)/event/$', views.del_from_cart_event, name='del_from_cart_event'),
+    url(r'^del/(?P<id>\d+)/onedayclass/$', views.del_from_cart_onedayclass, name='del_from_cart_onedayclass'),
+
 ]
